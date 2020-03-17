7b19996c36b3efcc23fee3c1c4903c58906bd9d6
Wise Duho <duhowise@gmail.com>
2020-03-17 11:34:46 +0000

    using multiple triggers for the same job

--- QuartzJobs/Controllers/JobsController.cs
+++ QuartzJobs/Controllers/JobsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Quartz;
 using QuartzJobs.Jobs;
@@ -17,16 +18,37 @@ namespace QuartzJobs.Controllers
         }
       [Route("")][HttpPost]  public async Task<IActionResult> StartSimpleJob()
         {
-            var simpleJobDetail  = JobBuilder.Create<SimpleJob>().UsingJobData("username","some data").UsingJobData("password","somePass").WithIdentity(nameof(SimpleJob), "Jobs").Build();
+            var simpleJobDetail  = JobBuilder.Create<SimpleJob>().UsingJobData("username","some data").UsingJobData("password","somePass").WithIdentity(nameof(SimpleJob), "Jobs").StoreDurably().Build();
             simpleJobDetail.JobDataMap.Put("user", new SimpleJobParameter
             {
                 Username = "NiceOne",
                 Password = "NiceOne"
 
             });
-            var trigger = TriggerBuilder.Create().UsingJobData("triggerParam","sample trigger data").WithIdentity($"{nameof(SimpleJob)}Trigger", "Jobs").StartNow()
+
+            await _scheduler.AddJob(simpleJobDetail, true);
+
+            var trigger = TriggerBuilder.Create()
+                .ForJob(simpleJobDetail)
+                .UsingJobData("triggerParam","sample trigger data")
+                .WithIdentity($"{nameof(SimpleJob)}Trigger", "Jobs")
+                .StartNow()
                 .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).WithRepeatCount(5)).Build();
-            await _scheduler.ScheduleJob(simpleJobDetail, trigger);
+            
+            
+            var trigger2 = TriggerBuilder.Create()
+                .ForJob(simpleJobDetail)
+                .UsingJobData("triggerParam", "sample trigger2 data")
+                .WithIdentity($"{nameof(SimpleJob)}Trigger2", "Jobs")
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithInterval(TimeSpan.FromSeconds(5)).RepeatForever()).Build();
+          
+            
+            
+            
+            
+            await _scheduler.ScheduleJob(trigger);
+            await _scheduler.ScheduleJob(trigger2);
             return Ok(simpleJobDetail);
         }
     }
